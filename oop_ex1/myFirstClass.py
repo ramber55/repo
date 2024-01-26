388e4b9bc4d3022d49d214671d87a3964887e722
ramber55 <ramonbernal1355@outlook.es>
2024-01-26 21:37:55 +0100

    código escrito

--- oop_ex1/myFirstClass.py
+++ oop_ex1/myFirstClass.py
@@ -1,5 +1,3 @@
-print("first class")
-
 # Tipos de Frutas
 tiposFruta = ("manzana", "pera", "plátano", "aguacate", "mandarina")
 manzana = 0
@@ -13,6 +11,8 @@ pequenio = 0
 medio = 1
 grande = 2
 
+# Clases -----------------------------------------------------------
+
 
 class Fruta:
     
@@ -21,7 +21,38 @@ class Fruta:
         self.tamanio = tamanio
 
     def __str__(self):
-        return f"{self.tipo} de tamaño {self.tamanio}"
+        txt = "{} de tamaño {}"
+        return txt.format(tiposFruta[self.tipo], tamanioFruta[self.tamanio])
+
+
+class Manzana (Fruta):
+
+    def __init__(self, tamanio):
+        super().__init__(manzana, tamanio)
+
+
+class Pera(Fruta):
+
+    def __init__(self, tamanio):
+        super().__init__(pera, tamanio)
+
+
+class Platano(Fruta):
+
+    def __init__(self, tamanio):
+        super().__init__(platano, tamanio)
+
+
+class Aguacate(Fruta):
+
+    def __init__(self, tamanio):
+        super().__init__(aguacate, tamanio)
+
+
+class Mandarina(Fruta):
+
+    def __init__(self, tamanio):
+        super().__init__(mandarina, tamanio)
 
 
 class Frutero:
@@ -38,3 +69,14 @@ class Frutero:
     def meter_fruta(self, fruta):
         self.frutas.append(fruta)
 
+
+# main ------------------------------------------------
+print("Here we go ...")
+manzana1 = Manzana(medio)
+manzana2 = Manzana(pequenio)
+print("una manzana:", manzana2)
+pera1 = Pera(grande)
+frutero = Frutero()
+frutero.meter_fruta(manzana1)
+frutero.meter_fruta(manzana2)
+frutero.meter_fruta(pera1)
