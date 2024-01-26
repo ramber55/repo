60a366774619a7dae2fee6ea5c31b1eda934ed81
ramber55 <ramonbernal1355@outlook.es>
2024-01-26 17:41:07 +0100

    first version

---
+++ 1stExample/ejemplo1.py
@@ -0,0 +1,9 @@
+print("Hello cruel world!...")
+
+print("Printing the first 20 integer non null numbers:")
+suma = 0
+for i in range(20):
+    print("->", i+1)
+    suma += i+1
+
+print("And their sum up is:", suma)
