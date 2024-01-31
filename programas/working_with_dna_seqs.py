aecbf6b98620b95bb0f5729f22a3b9090955055a
ramber55 <ramonbernal1355@outlook.es>
2024-01-31 22:59:59 +0100

    modificación

---
+++ programas/working_with_dna_seqs.py
@@ -0,0 +1,14 @@
+from dna_seq_mgmt import *
+
+DNA_REPOSITORY = "../recursos/"
+filename = "Homo_sapiens_ADA_sequence.fa"
+dna_header, dna_chain = seq_read_fasta(DNA_REPOSITORY + filename)
+
+print("DNA Header:", dna_header)
+print("---------------------------------------------------------------")
+print("DNA Seq   :", dna_chain)
+
+print("DNA file:", filename)
+print("The first 20 bases are:")
+print(dna_chain[:20])
+
