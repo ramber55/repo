241b3f8dd8e6191d57a4976bd0a6d5da68a9b9d6
ramber55 <ramonbernal1355@outlook.es>
2024-02-03 13:45:03 +0100

    Practicing with two DNA sequences

--- programas/working_with_dna_seqs.py
+++ programas/working_with_dna_seqs.py
@@ -4,17 +4,24 @@ from pprint import pprint
 
 
 DNA_REPOSITORY = Path.cwd().parent / "recursos"
-filename = "Homo_sapiens_ADA_sequence.fa"
-ADA_full_filename = DNA_REPOSITORY / filename
+
+ADA_filename = "Homo_sapiens_ADA_sequence.fa"
+ADA_full_filename = DNA_REPOSITORY / ADA_filename
+
+RNU6_269P_filename = "Homo_sapiens_RNU6_269P_sequence.fa"
+RNU6_269P_full_filename = DNA_REPOSITORY / RNU6_269P_filename
 
 ADA_sequence = DNA_SEQUENCE()
 ADA_sequence.read_seq_from_file(ADA_full_filename)
+
+RNU6_269P_sequende = DNA_SEQUENCE()
+RNU6_269P_sequende.read_seq_from_file(RNU6_269P_full_filename)
 
 print("DNA Header:", ADA_sequence.get_header())
 print("---------------------------------------------------------------")
 print("DNA Seq   :", ADA_sequence.get_body())
 
-print("DNA file:", filename)
+print("DNA file:", ADA_filename)
 print("The first 20 bases are:")
 print(ADA_sequence.get_body()[:20])
 
