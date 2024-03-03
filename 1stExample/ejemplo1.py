5b4610badd7b0d49b3c50091a5907043062485da
ramon <ramonbernal1355@outlook.es>
2024-03-03 17:57:03 +0100

    desde la máquina linux

--- 1stExample/ejemplo1.py
+++ 1stExample/ejemplo1.py
@@ -2,7 +2,7 @@ print("Hello cruel world from a Linux Machine!...")
 
 print("Printing the first 20 integer non null numbers:")
 suma = 0
-for i in range(20):
+for i in range(100):
     print("->", i+1)
     suma += i+1
 
