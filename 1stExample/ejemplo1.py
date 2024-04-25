0e69529d5495c671ef6bf5547828ce0cedb456f7
andreamartinbernal <a.martinb.2023@alumnos.urjc.es>
2024-04-25 15:31:17 +0200

    hgftrdrut

--- 1stExample/ejemplo1.py
+++
@@ -1,9 +0,0 @@
-print("Hello cruel world from a Linux Machine!...")
-
-print("Printing the first 20 integer non null numbers:")
-suma = 0
-for i in range(100):
-    print("->", i+1)
-    suma += i+1
-
-print("And their sum up is:", suma)
